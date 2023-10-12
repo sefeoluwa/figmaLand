a4cc77104b34ea764a8e153da7e021984301ef62
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-12 19:15:25 +0100

    added components

---
+++ src/components/Try.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Try() {
+  return (
+    <div>Try</div>
+  )
+}
+
+export default Try
---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Contents from './Contents.jsx'
+import Navbar from './Navbar.jsx'
+import Hero from './Hero.jsx'
+import Features from './Features.jsx'
+import Footer from './Footer.jsx'
+import Gallery from './Gallery.jsx'
+import Partners from './Partners.jsx'
+import Try from './Try.jsx'
+import Testimonials from './Testimonials.jsx'
+
+
+export {
+    Contents,
+    Navbar,
+    Hero,
+    Features,
+    Footer,
+    Gallery,
+    Partners,
+    Testimonials,
+    Try,
+}
