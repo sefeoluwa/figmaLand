493dbdedbd7619fc6ed261054b759ac142ee5e11
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-12 19:32:38 +0100

    added image files

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import airbnb from './airbnb.png'
+import alien from './alien.png'
+import android from './android.png'
+import apiary from './apiary.png'
+import apple from './apple.png'
+import avatarA from './avatarA.png'
+import avatarB from './avatarB.png'
+import avatarC from './avatarC.png'
+import avatarD from './avatarD.png'
+import basecamp from './basecamp.png'
+import Boards from './Boards.png'
+import Card1 from './Card1.png'
+import Card2 from './Card2.png'
+import Card3 from './Card3.png'
+import Card4 from './Card4.png'
+import CardA from './CardA.png'
+import CardB from './CardB.png'
+import CardC from './CardC.png'
+import CardD from './CardD.png'
+import forks from './forks.png'
+import ibm from './ibm.png'
+import infinity from './infinity.png'
+import landing from './landing.png'
+import logo from './logo.png'
+import Macbook from './Macbook.png'
+import Panel from './Panel.png'
+import screens from './screens.png'
+
+
+export default {
+    airbnb,
+    alien,
+    android,
+    apiary,
+    apple,
+    avatarA,
+    avatarB,
+    avatarC,
+    avatarD,
+    basecamp,
+    Boards,
+    Card1,
+    Card2,
+    Card3,
+    Card4,
+    CardA,
+    CardB,
+    CardC,
+    CardD,
+    forks,
+    ibm,
+    infinity,
+    landing,
+    logo,
+    Macbook,
+    Panel,
+    screens,
+}
