63b53f8b16d2218d07e79c4b55f3e0711acb06b5
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 14:33:43 +0100

    rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Navbar, Hero, Footer, Gallery, Features, Try, Contents, Partners, Testimonials } from './components'
+
 
 function App() {
  
 
   return (
-    <>
-     
-    </>
+    <div className='m-0 p-0 box-border bg-black text-white'>
+     <Navbar />
+     <Hero />
+     <Features />
+     <Contents />
+     <Gallery />
+     <Partners />
+     <Testimonials />
+     <Try />
+     <Footer />
+    </div>
   )
 }
 
