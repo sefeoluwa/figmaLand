e93dd1ab00fd7ebaebaf03b6d9d1f895c75120f3
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 15:10:56 +0100

    basic nav styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { navlinks } from '../constants'
 
 function Navbar() {
   return (
-    <nav className=''>
-      <img src={logo} alt="" />
-      <div className="">
+    <nav className='flex justify-around sticky top-0 '>
+      <img src={logo} alt="website logo" className='w-[15%] h-16 cursor-pointer' />
+      <div className="flex items-center justify-between w-[30%]">
         {navlinks.map((link) => (
           <div className="" key={link.name}>
             <a href="#" className='list-none'>{link.title}</a>
           </div>
         ))}
       </div>
-      <button>Login</button>
+      <button className='w-[139px] h-[50px] rounded-[7px] border-[1px] mt-2'>Login</button>
     </nav>
   )
 }
