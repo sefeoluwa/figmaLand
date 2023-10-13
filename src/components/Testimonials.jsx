f595b662cf6b27798e0ac1d6eca3557972dd58dd
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 21:01:08 +0100

    rendered testimonials and styles

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { testimonials } from "../constants"
+
 
 function Testimonials() {
   return (
-    <div>Testimonials</div>
+    <div className='flex mt-[200px] overflow-hidden font-sans  flex-col p-[65px 50px] gap-14 justify-center items-center'>
+      <h2 className='text-[52px] leading-[62px]'>Testimonials</h2>
+     <div className="testimonial grid grid-cols-2 gap-4 ">
+     {testimonials.map((rate) => (
+        <div className="w-[430px] h-[328px] gap-[30px] p-[40px] border-[1px] border-[#DEDEDE] border-solid rounded-[10px]" key={rate.name}>
+          <div className="flex gap-5">
+            <img src={rate.icon} alt="" />
+            <div className="">
+              <p>{rate.name}</p>
+              <p>{rate.role}</p>
+            </div>
+          </div>
+          <p className="w-[320px] h-[168px] mt-7 text-[16px] leading-[24px] tracking-[0.2px]">{rate.review}</p>
+        </div>
+      ))}
+     </div>
+    </div>
   )
 }
 
