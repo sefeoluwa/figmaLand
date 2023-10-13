ea490b17ad1dab2f290539be2f2f975f1cf64f34
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 18:37:53 +0100

    added margin top to section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import { Panel } from '../assets'
 import { features } from '../constants'
 
 function Features() {
   return (
-    <div className='flex  font-sans  flex-col p-[65px 50px] gap-10 justify-center items-center'>
+    <div className='flex mt-[100px] font-sans  flex-col p-[65px 50px] gap-10 justify-center items-center'>
       <h2 className='text-[52px] leading-[62px]'>Features</h2>
       <p className='text-[20px] flex items-center leading-[30px] tracking-[0.2px] w-[700px] h-[60px] p-[20px] pl-[60px]'>Most calendars are designed for teams. Slate is designed for 
 freelancers who want a simple way to plan their schedule.</p>
-<div className="flex justify-center flex-wrap p-5">
+<div className="flex justify-center flex-wrap gap-6 p-5">
   <img src={Panel} alt="" className='w-[55%]'/>
   <div className="flex flex-col items-center gap-3">
     {features.map((feat) => (
