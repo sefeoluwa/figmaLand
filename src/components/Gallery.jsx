f50099cc7e520a09f37fe6987568d3d9dccef0f9
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 20:00:09 +0100

    rendered images with styles

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+
+import { galleryBottom, galleryTop } from "../constants"
+
 
 function Gallery() {
   return (
-    <div>Gallery</div>
+    <div className='flex mt-[200px] font-sans  flex-col p-[65px 50px] gap-14 justify-center items-center'>
+      <h2 className='text-[52px] leading-[62px]'>Gallery</h2>
+      <p className="text-[20px] flex items-center leading-[30px] tracking-[0.2px] w-[571px] text-center h-[60px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
+      <div className="flex items-center w-[999px] flex-wrap justify-center">
+        {galleryTop.map((top) => (
+          <div className="" key={top.name}>
+            <img className="w-[225px] h-[285px]" src={top.icon} alt="" />
+          </div>
+        ))}
+      </div>
+      <div className="galleryBottom flex items-center justify-center flex-wrap h-[285px]">
+        {galleryBottom.map((bottom) => (
+          <div className="" key={bottom.name}>
+            <img className="" src={bottom.icon} alt="" />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
