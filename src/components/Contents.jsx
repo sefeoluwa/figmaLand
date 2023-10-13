a2624454c3d3cb7a331a98e1327affdc42a8b3e9
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 18:39:47 +0100

    headin and subheading with styles

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+
+
 
 function Contents() {
   return (
-    <div>Contents</div>
+    <div className='flex mt-[200px] font-sans  flex-col p-[65px 50px] gap-10 justify-center items-center'>
+      <h2 className='text-[52px] leading-[62px]'>Contents</h2>
+      <p className="text-[20px] flex items-center leading-[30px] tracking-[0.2px] w-[571px] text-center h-[60px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
+    </div>
   )
 }
 
