f86de7faf2759c07095426f2f7021bbdecbe2181
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-13 19:23:18 +0100

    styles for content cards

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -6,12 +6,12 @@ import { content } from "../constants"
 
 function Contents() {
   return (
-    <div className='flex mt-[200px] font-sans  flex-col p-[65px 50px] gap-10 justify-center items-center'>
+    <div className='flex mt-[200px] font-sans  flex-col p-[65px 50px] gap-14 justify-center items-center'>
       <h2 className='text-[52px] leading-[62px]'>Contents</h2>
       <p className="text-[20px] flex items-center leading-[30px] tracking-[0.2px] w-[571px] text-center h-[60px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
-      <div className="flex gap-10 items-center justify-center flex-wrap">
+      <div className=" flex gap-10 items-center justify-center flex-wrap">
         {content.map((content) => (
-          <div className="flex h-[624px] rounded-[10px] py-[35px] text-black bg-white flex-col items-center pr-[20px] pl-[20px] w-[476px] gap-[57px]" key={content.name}>
+          <div className="contentCard flex h-[624px] rounded-[10px] py-[35px] text-black bg-white flex-col items-center pr-[20px] pl-[20px] w-[476px] gap-[57px]" key={content.name}>
             <p className="font-bold text-[20px] leading-[30px] tracking-[0.1px]">{content.name}</p>
             <p className="text-center w-[268px] h-[72px] leading-[24px] tracking-[0.2px] text-[16px] p-1">{content.text}</p>
             <div className="">
