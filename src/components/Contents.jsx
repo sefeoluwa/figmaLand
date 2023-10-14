0ddcdac0e29e5dd82f5c586bba04b3fbbbdc6abc
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 12:57:21 +0100

    card styles for smaller screens

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -8,10 +8,10 @@ function Contents() {
   return (
     <div className='flex mt-[200px] font-sans  flex-col p-[65px 50px] gap-14 justify-center items-center'>
       <h2 className='text-[52px] leading-[62px]'>Contents</h2>
-      <p className="text-[20px] flex items-center leading-[30px] tracking-[0.2px] w-[571px] text-center h-[60px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
+      <p className=" xs:mt-[10px] sm:mt-[10px] text-center md:mt-3 text-[20px] sm:w-[411px] sm:h-[90px] flex items-center leading-[30px] tracking-[0.2px] md:w-[700px] md:h-[60px] p-[20px] pl-[30px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
       <div className=" flex gap-10 items-center justify-center flex-wrap">
         {content.map((content) => (
-          <div className="contentCard flex h-[624px] rounded-[10px] py-[35px] text-black bg-white flex-col items-center pr-[20px] pl-[20px] w-[476px] gap-[57px]" key={content.name}>
+          <div className="contentCard flex md:h-[624px] xs:w-[348px] xs:h-[493px] xs:gap-10 rounded-[10px] py-[35px] text-black bg-white flex-col items-center pr-[20px] pl-[20px] md:w-[476px] gap-[57px]" key={content.name}>
             <p className="font-bold text-[20px] leading-[30px] tracking-[0.1px]">{content.name}</p>
             <p className="text-center w-[268px] h-[72px] leading-[24px] tracking-[0.2px] text-[16px] p-1">{content.text}</p>
             <div className="">
