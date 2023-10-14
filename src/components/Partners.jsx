7eab443e488e1eb13ae97c58a3b191e28eeccdb9
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 13:02:31 +0100

    sub heading style and padding for icons

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -6,8 +6,8 @@ function Partners() {
   return (
     <div className='flex mt-[200px] font-sans  flex-col p-[65px 50px] gap-14 justify-center items-center'>
       <h2 className='text-[52px] leading-[62px]'>Partners</h2>
-      <p className="text-[20px] flex items-center leading-[30px] tracking-[0.2px] w-[571px] text-center h-[60px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
-      <div className="flex gap-[50px] flex-wrap items-center justify-center">
+      <p className="xs:mt-[10px] sm:mt-[10px] text-center md:mt-3 text-[20px] sm:w-[411px] sm:h-[90px] flex items-center leading-[30px] tracking-[0.2px] md:w-[700px] md:h-[60px] p-[20px] pl-[30px]">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
+      <div className="flex gap-[50px] flex-wrap items-center justify-center px-5">
         {partners.map((part, index) => (
           <img src={part.icon} key={index} alt="" />
         ))}
