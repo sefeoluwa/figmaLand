4c71eb288b56fb00bcb78081ea62ebe9e3551341
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 15:46:28 +0100

    reduced md screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
       screens: {
         xs: "450px",
         sm: '640px',
-        md: "768px",
+        md: "710px",
         lg: '1024px',
         xl: '1280px',
         xxl: '1536px',
