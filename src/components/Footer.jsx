b30413056ee45622d91c659d1e74943e272dd7f9
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 14:46:06 +0100

    footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,37 +5,36 @@ import { ImFacebook2 } from 'react-icons/im'
 
 function Footer() {
   return (
-    <div className="footer mt-[300px] pb-[70px] flex justify-around md:gap-1 xs:gap-[100px] flex-wrap p-5">
-      <div className="foot-links xs:flex-col md:flex-row flex flex-wrap xs:gap-7 md:gap-1 font-sans justify-around w-[40%]">
-      {footer.map((foot) => (
-        <div className="" key={foot.name}>
-          <p className='font-bold text-[20px] '>{foot.name}</p>
-          <ul className="flex flex-col gap-3 mt-7">
-            {foot.links.map((link) => (
-              <li key={link} className='text-[16px] cursor-pointer'>{link}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-      <div className="font-sans text-[16px] flex flex-col gap-5">
-        <div className="flex gap-3">
-          <MdOutlineLocationOn size='30px'/>
+    <div className="footer font-sans flex-wrap px-5 flex flex-col md:flex-row justify-around  mt-[300px] pb-[70px]  items-center">
+      <div className="foot-links flex flex-col md:flex-row text-[20px] md:text-4xl w-[40%] lg:gap-12 justify-around md:w-auto mb-10 items-center  md:mb-0">
+        {footer.map((foot) => (
+          <div className="mb-6 md:mb-0 md:mr-10" key={foot.name}>
+            <p className="font-bold text-[20px]  mb-3 md:mb-4">{foot.name}</p>
+            <ul className="flex flex-col gap-3">
+              {foot.links.map((link) => (
+                <li key={link} className="text-[16px] cursor-pointer">{link}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="font-sans flex flex-col gap-7 text-[16px]text-center">
+        <div className="flex items-center gap-3 mb-6 md:mb-0">
+          <MdOutlineLocationOn size='30px' />
           <p className='mt-1'>7480 Mockingbird Hill undefined</p>
         </div>
-        <div className="flex gap-3">
-          <MdPhoneAndroid size='30px'/>
+        <div className="flex items-center gap-3 mb-6 md:mb-0">
+          <MdPhoneAndroid size='30px' />
           <p className='mt-1'>(239) 555-0108</p>
         </div>
-        <div className="flex mt-6 gap-10">
-          <BsTwitter size='30px' cursor='pointer'/>
+        <div className="flex items-center gap-6">
+          <BsTwitter size='30px' cursor='pointer' />
           <ImFacebook2 size='30px' cursor='pointer' />
           <BsLinkedin size='30px' cursor='pointer' />
         </div>
       </div>
     </div>
   );
-
 }
 
 export default Footer;
