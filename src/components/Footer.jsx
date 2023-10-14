cefec68d33a2fa39ba05f3e9b3add3ce6af08896
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 14:22:34 +0100

    mobile reponsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import { ImFacebook2 } from 'react-icons/im'
 
 function Footer() {
   return (
-    <div className="flex justify-around flex-wrap ">
-      <div className="flex flex-wrap font-sans justify-around mt-[300px] pb-[100px] w-[40%]">
+    <div className="footer mt-[300px] pb-[70px] flex justify-around md:gap-1 xs:gap-[100px] flex-wrap p-5">
+      <div className="foot-links xs:flex-col md:flex-row flex flex-wrap xs:gap-7 md:gap-1 font-sans justify-around w-[40%]">
       {footer.map((foot) => (
         <div className="" key={foot.name}>
-          <p className='font-bold text-[20px]'>{foot.name}</p>
+          <p className='font-bold text-[20px] '>{foot.name}</p>
           <ul className="flex flex-col gap-3 mt-7">
             {foot.links.map((link) => (
               <li key={link} className='text-[16px] cursor-pointer'>{link}</li>
@@ -18,7 +18,7 @@ function Footer() {
         </div>
       ))}
     </div>
-      <div className="mt-[300px] pb-[100px] font-sans text-[16px] flex flex-col gap-5">
+      <div className="font-sans text-[16px] flex flex-col gap-5">
         <div className="flex gap-3">
           <MdOutlineLocationOn size='30px'/>
           <p className='mt-1'>7480 Mockingbird Hill undefined</p>
