42e95c1b2593d317c5a91fe38b433fa694ea6552
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 14:53:26 +0100

    fixed padding for cards

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ function Testimonials() {
   return (
     <div className='flex mt-[200px] overflow-hidden font-sans  flex-col p-[65px 50px] gap-14 justify-center items-center'>
       <h2 className='text-[52px] leading-[62px]'>Testimonials</h2>
-     <div className="testimonial grid grid-cols-2 gap-4 ">
+     <div className="testimonial grid grid-cols-2 gap-4 p-10 md:p-1 ">
      {testimonials.map((rate) => (
         <div className="md:w-[430px] md:h-[328px] xs:w-[272px] sm:h-[296px] gap-[30px] p-[40px] border-[1px] border-[#DEDEDE] border-solid rounded-[10px]" key={rate.name}>
           <div className="flex gap-5">
