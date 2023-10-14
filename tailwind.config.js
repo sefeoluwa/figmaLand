ef9c978d495b1bcdbed396af5a27710f9335942d
sefeoluwa <sefeoluwa01@gmail.com>
2023-10-14 12:21:45 +0100

    added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ export default {
     extend: {
       colors: {
         primary: '#4452FE',
-      }
+      },
+      screens: {
+        xs: "450px",
+        sm: '640px',
+        md: "768px",
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1536px',
+      },
+  
     },
   },
   plugins: [],
